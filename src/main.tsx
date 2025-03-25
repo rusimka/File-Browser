057b9f47b197f16c70137edd5888d2d72593df48
rusimka <rusimkad@gmail.com>
2025-03-25 22:00:59 +0100

    - set up the version the compatible versions on react, chonky and material ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import * as ReactDOM from 'react-dom';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const App = () => {
+    return (
+        <div style={{ height: 400 }}>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
